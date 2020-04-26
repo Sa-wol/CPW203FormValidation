95e588e12c530f650b6e5b76d364962caeb1506e
April Dunn <aprild@Aprils-MacBook-Pro.local>
2020-04-26 00:01:12 -0700

    Created function to test if textbox have data

--- js/main.js
+++ js/main.js
@@ -3,5 +3,16 @@ window.onload = function () {
     formBtn.onclick = main;
 };
 function main() {
-    alert("Reg button was clicked");
+    isTextPresent("first-name", "First name is required");
+    isTextPresent("last-name", "Last name is required");
+}
+function isTextPresent(id, errMsg) {
+    var txtBox = document.getElementById(id);
+    var textBoxValue = txtBox.value;
+    if (textBoxValue == "") {
+        var errSpan = txtBox.nextElementSibling;
+        errSpan.innerText = errMsg;
+        return false;
+    }
+    return true;
 }
--- ts/main.ts
+++ ts/main.ts
@@ -4,5 +4,24 @@ window.onload = function(){
 }
 
 function main():void{
-    alert("Reg button was clicked");
+    isTextPresent("first-name", "First name is required");
+    isTextPresent("last-name", "Last name is required");
+}
+
+/**
+ * Returns true if the textbox with the given id
+ * has some text inside it
+ * @param id The id of the <input type="text"> to validate
+ * @param errMsg The message to display in the sibling span
+ * of the textbox
+ */
+function isTextPresent(id:String, errMsg:string):boolean {
+    let txtBox = <HTMLInputElement>document.getElementById(id);
+    let textBoxValue = txtBox.value;
+    if (textBoxValue == "") {
+        let errSpan = <HTMLElement>txtBox.nextElementSibling;
+        errSpan.innerText = errMsg;
+        return false;
+    }
+    return true;
 }
